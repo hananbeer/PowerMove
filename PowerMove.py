34991c1d69fa6793b8e9c7bddde9cd13a5389300
Hanan Beer <email@hanan.beer>
2023-02-02 08:21:44 +0200

    temporarily limit hotkey to pose mode - can still invoke bpy.ops.transform.powermove directly

--- PowerMove.py
+++ PowerMove.py
@@ -11,14 +11,26 @@ bl_info = {
     #'doc_url': '',
     #'tracker_url': '',
     #'support': '',
-    'category': 'Tools'
+    'category': 'Animation'
 }
 
+OP_PowerMove = 'transform.powermove'
+
 class OBJECT_OT_powermove(bpy.types.Operator):
-    bl_idname = 'transform.powermove'
+    bl_idname = OP_PowerMove
     bl_label = 'PowerMove'
     bl_options = {'REGISTER', 'UNDO'}
     prev_xy = (0, 0)
+    is_running = False
+
+    # TODO: don't need to limit just to pose mode
+    # but for the initial version let's experiment just in pose
+    # (remember to restore bl_idname above to transform.powermove)
+    # @classmethod
+    # def poll(cls, context):
+    #     return (context.object and
+    #             context.mode == 'POSE' and
+    #             context.object.pose)
 
     def execute(self, context):
         return {'FINISHED'}
@@ -26,11 +38,12 @@ class OBJECT_OT_powermove(bpy.types.Operator):
     def modal(self, context, event):
         # ESC will finish the operation and so will double-clicking
         if event.type == 'ESC':
+            OBJECT_OT_powermove.is_running = False
             return {'FINISHED'}
 
         if event.type == 'LEFTMOUSE':
-            print('LEFTMOUSE xy: %s, %s' % self.prev_xy)
             if (event.mouse_x, event.mouse_y) == self.prev_xy:
+                OBJECT_OT_powermove.is_running = False
                 return {'FINISHED'}
         
             # allow selecting different objects/bones when pressing alt by returning PASS_THROUGH instead
@@ -47,21 +60,26 @@ class OBJECT_OT_powermove(bpy.types.Operator):
 
             # prevent context menu from opening on right click
             return {'RUNNING_MODAL'}
-            
+
         return {'PASS_THROUGH'}
 
     def invoke(self, context, event):
+        # prevent running operator multiple times (e.g. pressing 'G' while operator running)
+        if OBJECT_OT_powermove.is_running:
+            return {'CANCELLED'}
+
+        OBJECT_OT_powermove.is_running = True
         self.prev_xy = (0, 0)
-        #print('This is the invoker')
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
 addon_keymaps = []
 def init_keymaps():
     kc = bpy.context.window_manager.keyconfigs.addon
-    km = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+    # TODO: currently limiting keymap only to pose, but you can still invoke via transform.powermove 
+    km = kc.keymaps.new(name='Pose')
     kmi = [
-        km.keymap_items.new('transform.powermove', type='G', value='PRESS'),
+        km.keymap_items.new(OP_PowerMove, type='G', value='PRESS'),
     ]
     return km, kmi
 
@@ -74,9 +92,10 @@ def register():
         for k in kmi:
             k.active = True
             addon_keymaps.append((km, k))
-    
 
 def unregister():
+    # TODO: figure out why blender (3.1) does not call unregister...
+    print('unreg')
     global addon_keymaps
     for km, kmi in addon_keymaps:
         km.keymap_items.remove(kmi)
@@ -84,5 +103,4 @@ def unregister():
     addon_keymaps.clear()
     bpy.utils.unregister_class(OBJECT_OT_powermove)
 
-if __name__ == '__main__':
-    register()
+register()
